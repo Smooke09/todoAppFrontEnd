82ae8230e7df425309484c905cc38862d69e4710
Smooke09 <pedrolucas1014zz@gmail.com>
2022-06-20 12:04:13 -0300

    defining routes

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,17 +1,17 @@
+import React from "react";
+import Menu from "../template/menu";
+import Routes from "./routes";
+
 import "modules/bootstrap/dist/css/bootstrap.min.css";
 import "modules/font-awesome/css/font-awesome.min.css";
 
-import React from "react";
-import Todo from "../todo/todo";
-import About from "../about/about";
-import Menu from "../template/menu";
-
-export default (props) => {
+const App = (props) => {
   return (
     <div className="container">
       <Menu />
-      <Todo />
-      <About />
+      <Routes />
     </div>
   );
 };
+
+export default App;
