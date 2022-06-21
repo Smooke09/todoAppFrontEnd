35a6bf04101b1b26d88667868f2f9f916d97d1af
Smooke09 <pedrolucas1014zz@gmail.com>
2022-06-21 09:02:29 -0300

    onChange Event

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -9,11 +9,17 @@ const TodoForm = (props) => (
         id="description"
         className="form-control"
         placeholder="Adicione uma tarefa"
+        onChange={props.handleChange}
+        value={props.description}
       ></input>
     </Grid>
 
     <Grid cols="12 3 2">
-      <IconButton btnStyle="primary" icon="plus"></IconButton>
+      <IconButton
+        btnStyle="primary"
+        icon="plus"
+        onClick={props.handleAdd}
+      ></IconButton>
     </Grid>
   </div>
 );
